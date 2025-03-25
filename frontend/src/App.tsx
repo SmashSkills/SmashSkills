4538a1259adb308cae7bf0169264e1d8d5425f2a
LinoDM95 <linodemarco95@gmail.com>
2025-03-25 20:49:29 +0100

    set up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dignissimos
+        recusandae ex repellendus nesciunt atque libero reprehenderit
+        repudiandae sed maiores nam in deleniti quod sit ea veniam delectus,
+        sequi alias.
+      </h1>
+      <p className="">Test Text</p>
+    </div>
+  );
+}
+
+export default App;

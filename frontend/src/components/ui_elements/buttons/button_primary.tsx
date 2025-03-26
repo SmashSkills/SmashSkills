96974fe49a85b67f73f3a359655485d9954bfcd7
LinoDM95 <linodemarco95@gmail.com>
2025-03-26 10:01:03 +0100

    minor adjustments

--- frontend/src/components/ui_elements/buttons/button_primary.tsx
+++ frontend/src/components/ui_elements/buttons/button_primary.tsx
@@ -1,11 +1,18 @@
+type ButtonType = "button" | "submit" | "reset";
 interface ButtonPrimaryProps {
   title: string;
+  type?: ButtonType;
   className?: string;
 }
 
-const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({ title, className }) => {
+const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({
+  title,
+  type = "button",
+  className,
+}) => {
   return (
     <button
+      type={type}
       className={`font-inter text-white bg-primary rounded-full px-10 py-2 
         hover:font-semibold hover:bg-primary-hover 
         transition-colors duration-300
--- frontend/src/components/ui_elements/buttons/button_secondary.tsx
+++ frontend/src/components/ui_elements/buttons/button_secondary.tsx
@@ -1,15 +1,20 @@
+type ButtonType = "button" | "submit" | "reset";
+
 interface ButtonSecondaryProps {
   title: string;
+  type?: ButtonType;
   className?: string;
 }
 
 const ButtonSecondary: React.FC<ButtonSecondaryProps> = ({
   title,
+  type = "button",
   className,
 }) => {
   return (
     <button
-      className={`font-inter text-primary border-1 border-primary rounded-full px-10 py-2 
+      type={type}
+      className={`font-inter text-primary border border-primary rounded-full px-10 py-2 
         hover:font-semibold hover:border-primary-hover hover:text-primary-hover
         transition-colors duration-300
         ${className}`}
